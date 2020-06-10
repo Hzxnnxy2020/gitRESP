5e0740bdeaab71ab301d793c647e63c16039d2bc
Hzxnnxy2020 <2024297804@qq.com>
2020-06-10 11:01:35 +0800

    对用户模块进行DDL操作

---
+++ git-project/src/com/git/userdao/userdao.java
@@ -0,0 +1,22 @@
+package com.git.userdao;
+
+import java.util.List;
+
+public class userdao {
+  
+	public  void addUser(int id,String username,String password) {
+		//添加操作
+	}
+	public void deleteUser(int id) {
+		//删除操作
+	}
+	public void updateUser(int id,String newUsername,String newpassword) {
+		//修改操作
+	}
+	public List<Object> getUsers(int pageIndex,int pageSize){
+		
+		return null;
+	}
+	
+	}
+
