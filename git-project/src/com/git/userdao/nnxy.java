c0aa9890fe2ec5647a9582b81d9fa6a6875bf529
Hzxnnxy2020 <2024297804@qq.com>
2020-06-24 11:22:46 +0800

    add444

---
+++ git-project/src/com/git/userdao/nnxy.java
@@ -0,0 +1,22 @@
+package com.git.userdao;
+
+public class nnxy {
+  int id;
+  char name;
+@Override
+public String toString() {
+	return "nnxy [id=" + id + ", name=" + name + "]";
+}
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public char getName() {
+	return name;
+}
+public void setName(char name) {
+	this.name = name;
+}
+}
